8902ae6420968bea8fb476233b53ef0f8ef79c60
RicHincapie <hincapiet.ricardo@gmail.com>
2024-03-05 15:28:24 +1100

    Add operator, rbac, crd sample

--- internal/controller/namespaceconfig_controller.go
+++ internal/controller/namespaceconfig_controller.go
@@ -29,6 +29,7 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/log"
 
 	ricv1 "github.com/RicHincapie/ns-operator/api/v1"
+	namespaceHlp "github.com/RicHincapie/ns-operator/pkg/namespace"
 )
 
 // NamespaceConfigReconciler reconciles a NamespaceConfig object
@@ -40,6 +41,7 @@ type NamespaceConfigReconciler struct {
 //+kubebuilder:rbac:groups=ric.ric.com,resources=namespaceconfigs,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=ric.ric.com,resources=namespaceconfigs/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=ric.ric.com,resources=namespaceconfigs/finalizers,verbs=update
+//+kubebuilder:rbac:groups="",resources=namespaces,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
@@ -62,47 +64,98 @@ func (r *NamespaceConfigReconciler) Reconcile(ctx context.Context, req ctrl.Requ
 	_ = log.FromContext(ctx)
 
 	// THIS IS THE ACTUAL CRD SCHEMA TO LOAD THE CRD FOUND IN THE CLUSTER
-	instance := &ricv1.NamespaceConfig{}
-	err := r.Get(ctx, req.NamespacedName, instance)
-	if err != nil {
-		if errors.IsNotFound(err) {
-			log.Log.Info("NamespaceConfig resource not found. Ignoring since object must be deleted")
-			return ctrl.Result{}, nil
-		}
-	}
-	// TODO(user): your logic here
+	crdInstance := &ricv1.NamespaceConfig{}
 	//DEFINE THE NAMESPACE OBJECT
 	var namespace corev1.Namespace
-	// GET THE CRD NAME, WHICH IS THE NAMESPACE NAME WE WANT
-	namespaceChecked := instance.Name
-	prefix := instance.Spec.NamespacePrefix
-	nsFullName := prefix + namespaceChecked
-	// LOOKS FOR NS AND PLACE IT IN namespace
+	const crdFinalizer string = "namespaceConfig.finalizer.ric.com"
+
+	workingNs := &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{},
+	}
+	// LOAD THE CRD INTO INSTANCE
+	err := r.Get(ctx, req.NamespacedName, crdInstance)
+	// 	COMMENTED OUT BECAUSE WE WILL BE CALLED 2 TIMES: BEFORE THE FINALIZER AND AFTER IT?
+	// if err != nil {
+	// 	if errors.IsNotFound(err) {
+	// 		deletedNsName := crdInstance.Spec.NamespacePrefix + req.NamespacedName.Name
+	// 		log.Log.Info("NamespaceConfig CRD " + deletedNsName + " will be deleted." +
+	// 			"Proceeding to clean up namespace.")
+	// 		// TODO: GRACEFULLY DELETE THE NS AND ITS CONTENT
+
+	// 		workingNs.ObjectMeta.Name = deletedNsName
+	// 		if err = r.Client.Delete(ctx, workingNs); err != nil {
+	// 			log.Log.Error(err, "Namespace could not be deleted: "+deletedNsName)
+	// 			return ctrl.Result{}, err
+	// 		}
+	// 		return ctrl.Result{}, nil
+	// 	}
+	// }
+	// ------------ SECOND IMPLEMENTATION ---------------
+	if err != nil {
+		if errors.IsNotFound(err) {
+			log.Log.Info("CRD " + req.NamespacedName.Name + " deleted")
+			return ctrl.Result{}, nil
+		}
+		log.Log.Info("Error with CRD "+req.NamespacedName.Name+" deletion. Err: ", err)
+		return ctrl.Result{}, err
+	}
+	nsFullName := namespaceHlp.GenerateNamespaceName(crdInstance.Name, crdInstance.Spec.NamespacePrefix)
+	// Check if is not being deleted and needs the finalizer field to be set
+	if crdInstance.ObjectMeta.DeletionTimestamp.IsZero() {
+		if !namespaceHlp.ContainsString(crdInstance.ObjectMeta.Finalizers, crdFinalizer) {
+			crdInstance.ObjectMeta.Finalizers = append(crdInstance.ObjectMeta.Finalizers, crdFinalizer)
+			if err := r.Update(ctx, crdInstance); err != nil {
+				log.Log.Error(err, "Could not add finalizer to "+req.NamespacedName.Name)
+			}
+		}
+	} else {
+		// CRD has a deletion timestamp. Clean up logic
+		workingNs.ObjectMeta.Name = nsFullName
+		if err := r.Delete(ctx, workingNs); err != nil{
+			log.Log.Error(err, "Namespace " + nsFullName + " could not be deleted")
+			return ctrl.Result{}, err
+		}
+		// Remove finalizer from CRD
+		crdInstance.ObjectMeta.Finalizers = namespaceHlp.RemoveString(crdInstance.ObjectMeta.Finalizers, crdFinalizer)
+		if err := r.Update(ctx, crdInstance); err != nil{
+			log.Log.Error(err, "Could not remove finalizer from " + crdInstance.Name)
+			return ctrl.Result{}, err
+		}
+	}
+
+	// ------------ END SECOND IMPLEMENTATION ---------------
+	// Verification and ns creation logic
+	labelsInCrd := crdInstance.Spec.Labels
+	workingNs.ObjectMeta.Name = nsFullName
+	workingNs.ObjectMeta.Labels = labelsInCrd
+	workingNs.ObjectMeta.Finalizers = append(workingNs.ObjectMeta.Finalizers, crdFinalizer)
+
 	err = r.Client.Get(ctx, types.NamespacedName{Name: nsFullName}, &namespace)
 	if err != nil {
 		if client.IgnoreNotFound(err) != nil {
-			log.Log.Error(err, "Failed to get Namespace")
+			log.Log.Error(err, "Error getting Namespace")
 			return ctrl.Result{}, err
 		}
-		log.Log.Info("Namespace does not exists", "Namespace ", nsFullName)
-		// HERE STARTS THE LOGIC TO CREATE THE NS BECAUSE IT DOES NOT EXISTS
-		labelsInCrd := instance.Spec.Labels
+		log.Log.Info("Namespace " + nsFullName + " does not exists")
 
-		newNamespace := &corev1.Namespace{
-			ObjectMeta: metav1.ObjectMeta{
-				Name:   nsFullName,
-				Labels: labelsInCrd,
-			},
-		}
-		if err = r.Create(ctx, newNamespace); err != nil {
-			log.Log.Error(err, "Namespace could not be created", nsFullName)
-		}
-		log.Log.Info("Namespace ", nsFullName, " successfully created.")
+		// Create ns because it does not exists
 
-		return ctrl.Result{}, err
+		if err = r.Create(ctx, workingNs); err != nil {
+			log.Log.Error(err, "Namespace could not be created: "+nsFullName)
+			return ctrl.Result{}, err
+		}
+		log.Log.Info("Namespace " + nsFullName + " created and labeled with " + namespaceHlp.MapToStrings(labelsInCrd))
 	}
 	// HERE GOES THE LOGIC TO CHECK IF THE LABELS ARE PROPERY SET TO AN EXISTING NS
-
+	labelsInLiveNs := namespace.Labels
+	labelsToUpdate := namespaceHlp.CompareMaps(labelsInCrd, labelsInLiveNs)
+	// Overwrites labels in ns
+	workingNs.ObjectMeta.Labels = labelsToUpdate
+	if err = r.Update(ctx, workingNs); err != nil{
+		log.Log.Error(err, "Could not update labels " +
+			namespaceHlp.MapToStrings(labelsToUpdate) +
+			" for " + nsFullName)
+	}
 	return ctrl.Result{}, nil
 }
 
